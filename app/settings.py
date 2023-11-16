38942432b3741536e191dcf07406f9a57c0188d8
k.stopczynska <k.stopczynska@gmail.com>
2023-11-16 09:42:53 +0100

    added UserManager for creating users and tangled it with user model and auth model

--- app/settings.py
+++ app/settings.py
@@ -131,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# use core User model for auth, not BaserUserModel
+AUTH_USER_MODEL = 'core.User'
--- core/models.py
+++ core/models.py
@@ -4,10 +4,20 @@ from django.contrib.auth.models import (
 	BaseUserManager,
 	PermissionsMixin)
 
+class UserManager(BaseUserManager):
+	def create_user(self, email, password=None, **extra_fields):
+		user = self.model(email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(max_length=255, unique=True)
 	name = models.CharField(max_length=255)
 	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
+
+	objects = UserManager()
 
 	USERNAME_FIELD = 'email'
