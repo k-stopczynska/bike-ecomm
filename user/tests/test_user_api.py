9646342681039ff42633031e49c8f9b1d95546f3
k.stopczynska <k.stopczynska@gmail.com>
2023-12-05 17:09:30 +0100

    added user tests with errors when user exists already and password is too short

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -40,4 +40,18 @@ class PublicUserAPITest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_password_too_short_error(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'test',
+            'name': 'Test Name'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload.email
+        ).exists()
+        self.assertFalse(user_exists)
+
     
